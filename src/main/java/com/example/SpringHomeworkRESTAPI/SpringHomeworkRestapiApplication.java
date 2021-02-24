01a9ad69f3d8c9daa521753ebf06710ea6042b65
Bogdan Ionut <mihailov.bogdan96@gmail.com>
2021-02-24 17:55:02 +0200

    First commit

---
+++ src/main/java/com/example/SpringHomeworkRESTAPI/SpringHomeworkRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringHomeworkRESTAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHomeworkRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHomeworkRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringHomeworkRESTAPI/SpringHomeworkRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringHomeworkRESTAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHomeworkRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
