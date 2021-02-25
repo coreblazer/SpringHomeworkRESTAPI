3c4e14ae1a0d01dfbddf086624d0cb85b3d9fcad
Bogdan Ionut <mihailov.bogdan96@gmail.com>
2021-02-25 09:01:34 +0200

    Finally got the message right!!!!

--- src/main/java/controllers/RestAPIController.java
+++ src/main/java/controllers/RestAPIController.java
@@ -50,27 +50,4 @@ public class RestAPIController {
         }
         return value;
     }
-
-    @RequiredArgsConstructor
-    class Summing{
-        public int getA() {
-            return a;
-        }
-
-        public void setA(int a) {
-            this.a = a;
-        }
-
-        public int getB() {
-            return b;
-        }
-
-        public void setB(int b) {
-            this.b = b;
-        }
-
-        private int a;
-        private int b;
-    }
-
 }
